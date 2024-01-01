52bff9f8ed9233bae80ac70442eb8dde96133f58
SarCraft <nathan.henaux30@gmail.com>
2024-01-01 15:56:40 +0100

    Init NextJS and Shadcn

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'BetterTierList',
+  description: 'Website for creating and sharing tier lists with other people.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
